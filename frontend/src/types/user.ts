3926032c271e00bf013821be791c27a3168cb405
steviesims <steviesims05@gmail.com>
2025-12-05 01:01:24 -0600

    add frontend endpoint and type for fetching role

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,3 +1,11 @@
 import { type AuthUser } from "./auth";
 
 export type User = AuthUser;
+
+export type Role = {
+  id: string;
+  name: string;
+  users: AuthUser[];
+  createdAt: string;
+  updatedAt: string;
+};
